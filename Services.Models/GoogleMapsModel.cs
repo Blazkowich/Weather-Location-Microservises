3182ee4fa059c06a1c344b886b0f7c647b36340f
Blazkowich <Oiluridze@gmail.com>
2023-12-29 11:30:33 -0800

    Added Icons and Style

---
+++ Services.Models/GoogleMapsModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Models
+{
+    public class GoogleMapsResponse
+    {
+        public string Status { get; set; } = string.Empty;
+        public List<GoogleMapsResult> Results { get; set; }
+    }
+
+    public class GoogleMapsResult
+    {
+        public GoogleMapsGeometry Geometry { get; set; }
+    }
+
+    public class GoogleMapsGeometry
+    {
+        public GoogleMapsLocation Location { get; set; }
+    }
+
+    public class GoogleMapsLocation
+    {
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+    }
+}
